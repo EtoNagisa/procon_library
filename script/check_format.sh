57928fb8a9820b1196ba980315a66bf369344b88
Eto Nagisa <taiga.tiger.tora@gmail.com>
2019-09-17 19:59:51 +0900

    FIX

--- script/check_format.sh
+++ script/check_format.sh
@@ -7,11 +7,16 @@ done
 
 for file in `find|grep .cpp`
 do
-    clang-format -style=file $file > $TEMPORARY_FILE_NAME >& /dev/null
-    if ! diff $file $TEMPORARY_FILE_NAME >& /dev/null; then
+    clang-format -style=file $file > $TEMPORARY_FILE_NAME
+    diff $file $TEMPORARY_FILE_NAME
+    EXIT_CODE=$?
+    if [ $EXIT_CODE = 1 ]; then
         echo $file is not formatted
         rm $TEMPORARY_FILE_NAME
         exit 1
+    elif [ $EXIT_CODE = 2 ]; then
+        echo unknown error
+        exit 2
     fi
 done
 rm $TEMPORARY_FILE_NAME
